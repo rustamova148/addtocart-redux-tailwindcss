5a1b9c607b6330a1e2b87dc2ea563de15c9befee
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2025-06-05 11:17:09 +0400

    responsiveness

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,8 +5,8 @@ import { useSelector } from "react-redux"
 const Layout = () => {
   const statusTabCart = useSelector(store => store.cart.statusTab);
   return (
-    <div className="bg-zinc-200">
-      <main className={`w-[970px] max-w-full m-auto p-5 transform transition-transform duration-500
+    <div className="bg-zinc-200 w-[100%] min-h-screen">
+      <main className={`min-w-full] m-auto p-5 transform transition-transform duration-500
       ${statusTabCart === false ? "" : "-translate-x-40"}
       `}>
        <Header />
--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -34,26 +34,27 @@ const Detail = () => {
   return (
     <div>
       <h2 className="text-3xl text-center">PRODUCT DETAIL</h2>
-      <div className="grid grid-cols-2 gap-5 mt-5">
+      <div className="flex flex-col lg:flex-row items-center gap-5 mt-5">
           <div>
-             <img src={detail.image} alt="" className="w-full"/>
+             <img src={detail.image} alt="detailimg" width={600} height={600} />
           </div>
           <div className="flex flex-col gap-5">
-            <h1 className="text-4xl uppercase font-bold">{detail.name}</h1>
+            <h1 className="text-[16px] sm:text-[20px] md:text-[35px] lg:text-[35px] uppercase font-bold">{detail.name}</h1>
             <p className="font-bold text-3xl">
               ${detail.price}
             </p>
-            <div className="flex gap-5">
+            <div className="flex gap-5 h-[70px]">
               <div className="flex gap-2 justify-center items-center">
-                <button className="bg-gray-100 w-10 h-full font-bold text-xl rounded-xl flex justify-center items-center" onClick={handleMinusQuantity}>-</button>
+                <button className="bg-gray-100 w-10 h-[50px] font-bold text-xl rounded-xl flex justify-center items-center" onClick={handleMinusQuantity}>-</button>
                 <span className="bg-gray-200 w-10 h-full font-bold text-xl rounded-xl flex justify-center items-center">{quantity}</span>
-                <button className="bg-gray-100 w-10 h-full font-bold text-xl rounded-xl flex justify-center items-center" onClick={handlePlusQuantity}>+</button>
+                <button className="bg-gray-100 w-10 h-[50px] font-bold text-xl rounded-xl flex justify-center items-center" onClick={handlePlusQuantity}>+</button>
               </div>
-              <button className="bg-slate-900 text-white px-7 py-3 rounded-xl shadow-2xl" onClick={handleAddToCart}>
+              <button className="bg-slate-900 text-white px-7 rounded-xl shadow-2xl
+              text-[12px] md:text-[13px] lg:text-[16px]" onClick={handleAddToCart}>
                 Add To Cart
               </button>
             </div>
-            <p>{detail.description}</p>
+            <p className="text-[13px] md:text-[16px] md:w-[510px] lg:text-[16px]">{detail.description}</p>
           </div>
       </div>
     </div>
